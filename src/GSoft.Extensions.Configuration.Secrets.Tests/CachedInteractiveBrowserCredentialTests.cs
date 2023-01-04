284025f25dcb0d93f5b95b5681ded5c7f0b8c305
Anthony Simmon <anthony.simmon@gsoft.com>
2023-01-04 16:15:22 -0500

    Typo

--- src/GSoft.Extensions.Configuration.Secrets.Tests/CachedInteractiveBrowserCredentialTests.cs
+++ src/GSoft.Extensions.Configuration.Secrets.Tests/CachedInteractiveBrowserCredentialTests.cs
@@ -5,7 +5,7 @@ public class CachedInteractiveBrowserCredentialTests
     [Fact]
     public void InteractiveBrowserCredential_Record_Property_Exists()
     {
-        // This should throw if the property info can no longer be access through reflection
+        // This should throw if the property info can no longer be accessed through reflection
         _ = CachedInteractiveBrowserCredential.InternalRecordProperty;
     }
 }
